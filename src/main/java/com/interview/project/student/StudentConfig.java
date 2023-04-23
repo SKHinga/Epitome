e9c59d7be5e15ec34e48b60315fddd0a4e395e73
Samuel Kinuthia Hinga <shinga@safaricom.co.ke>
2023-04-23 15:44:11 +0300

    Add Data layer

---
+++ src/main/java/com/interview/project/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.interview.project.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+                Student jane = new Student(
+                            "Jane",
+                            "jane.dowy@gmail.com",
+                            LocalDate.of(2000, JANUARY, 5),
+                            21
+                );
+                Student ken = new Student(
+                            "Kennedy",
+                            "kennedy.simiyu@gmail.com",
+                            LocalDate.of(1993, SEPTEMBER, 25),
+                            31
+                );
+
+                repository.saveAll(
+                        List.of(ken, jane)
+                );
+        };
+    }
+}
